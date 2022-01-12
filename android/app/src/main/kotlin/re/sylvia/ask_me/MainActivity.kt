2bf00f90b4f972b397fc73ca3e1be40ed331a068
paquiry <paquirysylvia974@gmail.com>
2022-01-12 08:43:30 +0400

    projet ask_me

---
+++ android/app/src/main/kotlin/re/sylvia/ask_me/MainActivity.kt
@@ -0,0 +1,6 @@
+package re.sylvia.ask_me
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class ask_me extends StatefulWidget {
+  const ask_me({Key? key}) : super(key: key);
+
+  @override
+  _ask_meState createState() => _ask_meState();
+}
+
+class _ask_meState extends State<ask_me> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
