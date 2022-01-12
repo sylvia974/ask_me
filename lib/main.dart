f249a2f0cd80845a187d4b22d4adb90a9c8d7702
paquiry <paquirysylvia974@gmail.com>
2022-01-12 09:50:51 +0400

    modification ask me avec onPressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,57 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-class ask_me extends StatefulWidget {
-  const ask_me({Key? key}) : super(key: key);
-
-  @override
-  _ask_meState createState() => _ask_meState();
-}
-
-class _ask_meState extends State<ask_me> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Flutter ask me',
+      home: const MyHomePage(title: 'Ask Me Anything'),
+    );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int change = Random().nextInt(5)+1;
+  void _incrementCounter() {
+    setState(() {
+      change = Random().nextInt(5)+1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child:
+           TextButton(
+             onPressed: () { _incrementCounter(); },
+             child: Image.asset('images/ball$change.png'
+                ),
+           ),
+      ),
+    );
+  }
+}
